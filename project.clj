dd700f01120f6e0c93daad0c9d41282485670eec
Simon Katz <sk@nomistech.com>
2015-05-24 10:42:04 +0100

    Upgrade to latest versions.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "5.2.0"]
-                 [midje "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [cheshire "5.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
--- test/play_with_cheshire/core_test.clj
+++ test/play_with_cheshire/core_test.clj
@@ -6,12 +6,12 @@
 (fact
   (generate-string {:foo "bar" :baz 5})
   =>
-  "{\"foo\":\"bar\",\"baz\":5}")
+  "{\"baz\":5,\"foo\":\"bar\"}")
 
 (fact
   (generate-string [{:foo "bar" :baz 5}])
   =>
-  "[{\"foo\":\"bar\",\"baz\":5}]")
+  "[{\"baz\":5,\"foo\":\"bar\"}]")
 
 (fact
   (generate-string {:foo "bar" :baz (java.util.Date. 0)})
@@ -26,7 +26,7 @@
 (fact
   (generate-string {:foo "bar" :baz {:eggplant [1 2 3]}} {:pretty true})
   =>
-  "{\n  \"foo\" : \"bar\",\n  \"baz\" : {\n    \"eggplant\" : [ 1, 2, 3 ]\n  }\n}")
+  "{\n  \"baz\" : {\n    \"eggplant\" : [ 1, 2, 3 ]\n  },\n  \"foo\" : \"bar\"\n}")
 
 (fact
   (generate-string {:foo "It costs £100"})
